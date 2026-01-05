2b86fb76b5b9ca5fff400966954036ee3311cd00
Sanjiv Lobo <13509204+Xndr7@users.noreply.github.com>
2026-01-05 11:45:39 +0530

    Update start_gui.sh

--- alpine_kindle_kual/start_gui.sh
+++ alpine_kindle_kual/start_gui.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -f /mnt/us/extensions/kindle_copyparty/kindle_copyparty.sh ] && [ -f /etc/upstart/kindle_copyparty.conf ] && [ -f /mnt/us/extensions/kindle_copyparty/kindle_copyparty.ext3 ] ; then
-	start kindle_copyparty
+	start /mnt/us/extensions/kindle_copyparty/kindle_copyparty
 else
 	fbink -pmh -y -5 "Error: Required files missing. Deploy Alpine first!"
 fi
