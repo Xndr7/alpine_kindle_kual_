cbd56952a3a853d9e12c151e3edacf35f263dee0
Sanjiv Lobo <13509204+Xndr7@users.noreply.github.com>
2026-01-05 15:55:53 +0530

    Update start_kindle_copyparty.sh

--- alpine_kindle_kual/start_kindle_copyparty.sh
+++ alpine_kindle_kual/start_kindle_copyparty.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -f /mnt/us/extensions/kindle_copyparty/kindle_copyparty.sh ] && [ -f /mnt/us/extensions/kindle_copyparty/start_kindle_copyparty.sh ] && [ -f /etc/upstart/kindle_copyparty.conf ] && [ -f /mnt/us/extensions/kindle_copyparty/kindle_copyparty.ext3 ] ; then
+if [ -f /mnt/us/extensions/kindle_copyparty/kindle_copyparty.sh ] && [ -f /mnt/us/extensions/kindle_copyparty_kual/start_kindle_copyparty.sh ] && [ -f /etc/upstart/kindle_copyparty.conf ] && [ -f /mnt/us/extensions/kindle_copyparty/kindle_copyparty.ext3 ] ; then
     /mnt/us/extensions/kterm/bin/kterm -e "sh /mnt/us/extensions/kindle_copyparty/start_kindle_copyparty.sh" -k 1 -o U -s 7
 else
 	fbink -pmh -y -5 "Error: Required files missing. Deploy Alpine first!"
